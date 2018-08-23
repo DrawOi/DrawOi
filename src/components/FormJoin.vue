1615b4218a73032125aeea92cc75cccc8cadba69
maharama <maramstratos@gmail.com>
2018-08-23 22:52:35 +0700

    initial commit

--- src/components/FormJoin.vue
+++ src/components/FormJoin.vue
@@ -4,17 +4,31 @@
       <label>Username</label>
       <input type="text" v-model="username" placeholder="Input your username" class="form-control">
       <br>
-      <button type="submit" class="btn btn-primary">JOIN</button>
+      <button type="submit" class="btn btn-primary" @click="submit">JOIN</button>
     </div>
   </form>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   name: 'form-join',
   data: () => ({
     username: ''
-  })
+  }),
+  methods: {
+    ...mapActions([
+      "addPlayer"
+    ]),
+    submit(){
+      let inputObj = {
+        name: this.username,
+        points: 0
+      }
+      this.addPlayer(inputObj)
+    }
+  }
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -1,16 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import database from './firebase.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    room: []
   },
   mutations: {
-
+    ASSIGN_PLAYER (state, payload){
+      console.log(state);
+      state.room.push(payload)
+      console.log(payload);
+      if (state.players.length !== 4) {
+        console.log('room full');
+      }else {
+      }
+    }
   },
   actions: {
-
+    addPlayer(context, payload){
+      context.commit("ASSIGN_PLAYER", payload)
+      // Event.preventDefault();
+      console.log(payload);
+      database.ref('player/').push().set(payload, function(){
+        if (err) {
+          console.log(err);
+        }else {
+        }
+      })
+    }
   }
 })
