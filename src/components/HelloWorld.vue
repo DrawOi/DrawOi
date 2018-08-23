2635d7c3d957aba977665ab29a417893dc98d38e
arisupriatna14 <arisupriatna703@gmail.com>
2018-08-23 20:13:49 +0700

    add layout form join

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <a href="https://www.google.com" class="btn btn-outline-primary">TESTING</a>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar></Navbar>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm12">
+          <div class="card text-center">
+            <h5 class="card-header">JOIN PictOi</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                  <form-join></form-join>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import Navbar from "@/components/Navbar.vue";
+import FormJoin from "@/components/FormJoin.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Navbar,
+    FormJoin,
+  },
+  data: () => ({
+    username: ''
+  }),
+};
 </script>
+
+<style scoped>
+  .container {
+    margin-top: 20px !important;
+  }
+</style>
+
