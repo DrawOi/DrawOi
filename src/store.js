ecf137c56f6296290072b375310f8093e8ea58a8
Richardson12345 <chuddywarrior@gmail.com>
2018-08-23 22:59:12 +0700

    added firebase

--- src/store.js
+++ src/store.js
@@ -1,16 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { app, database} from '@/firebase.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    pictures: []
 
   },
   mutations: {
 
   },
   actions: {
-
+    addPicture ({ commit, dispatch}, pictureObj) {
+      database().ref('picture/').push.set({
+        answer: pictureObj.answer,
+        image: pictureObj.image,
+        hint : pictureObj.hint
+      }, function ( err ) {
+        if ( err ) {
+          alert('error')
+        } else {
+          console.log ('success')
+        }
+      });
+    } 
   }
 })
